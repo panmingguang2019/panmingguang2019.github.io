124b5efc0362bd9f216fd10c6cd4e38fb3dd2361
panmingguang <414072243@qq.com>
2020-07-01 17:16:16 +0800

    ..

--- js/f.js
+++ js/f.js
@@ -8,16 +8,9 @@
 		 if (isMobile()) {
 			    location.replace(url)
 		 } else {
-			 var ss = '<div class="pure-g"><iframe scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" height="2440" src="'+url+'"></iframe></div>';
+			 var ss = '<h3>手机打开, 或右击新标签打开你懂的</h3><div class="pure-g"><iframe scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="40%" height="40" src="'+url+'"></iframe></div>';
 			 eval("document.write('" + ss + "');");
 		 }
 
 	}
 
-	
- 	setInterval(function () {
-           try {
-               document.getElementById("layout").style.display = "none";
-           } catch (e) { }
-
-       }, 100);
